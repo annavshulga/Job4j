2e3ea6ea0333e4e2bc571ff5f210fe91c1354fa8
annavshulga <annavalentinshul@mail.ru>
2018-12-05 19:32:59 +0300

    1.3. JavaDoc. Code convention[#87695]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,14 @@
 package ru.job4j.calculate;
+/**
+*Объект класса Calculate пока не знаю, для чего будет нужен.
+*@author Anna Shulga (annavalentinshul@mail.ru)
+*/
 public class Calculate {
+/**
+*Конструктор, вывод строки на консоль.
+*@param args - описание аргумента метода.
+*@return - строка "У таракана усики"
+*/
 	public static void main(String[] args){
 		System.out.println("Y tarakana ysiki");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+*Package for calculate task.
+*
+*@author Anna Shulga (annavalentinshul@mail.ru)
+*version 0.1
+*since 05.12.2018
+*/
+package ru.job4j.calculate;
