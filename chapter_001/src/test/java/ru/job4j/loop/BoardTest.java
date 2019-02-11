2dbd16ab9659633ef43b7b3fff1ee0f38e7f5668
annavshulga <annavalentinshul@mail.ru>
2019-02-11 14:20:49 +0300

    5.3 Шахматная достка и два теста

--- chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -1,4 +1,5 @@
 package ru.job4j.loop;
+import java.util.StringJoiner;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -10,5 +11,13 @@ public class BoardTest {
         String result = board.paint(5, 4);
         String line = System.lineSeparator();
         assertThat(result, is(String.format("X X X%s X X %sX X X%s X X %s", line, line, line, line)));
+    }
+    @Test
+    public void when3And7Then3ColsAnd7Rows() {
+        Board board = new Board();
+        String result = board.paint(6, 3);
+        String line = System.lineSeparator();
+        String expected = new StringJoiner(line, "", line).add("X X X ").add(" X X X").add("X X X ").toString();
+        assertThat(result, is(expected));
     }
 }
